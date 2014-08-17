84cbe9d68feb7e090d7e629a0c5d892758cee5e3
Wu Donghui <wudonghui@Wu-Donghuis-MacBook-Pro.local>
2014-08-17 19:41:54 +0800

    no sass compile coz snapCi fails

--- Gruntfile.js
+++ Gruntfile.js
@@ -488,20 +488,20 @@ module.exports = function(grunt) {
 
   // When sails is lifted in production
   grunt.registerTask('prod', [
-    // 'clean:dev',
-    // 'jst:dev',
-    // 'less:dev',
-    // 'copy:dev',
-    // 'coffee:dev',
-    // 'concat',
-    // 'uglify',
-    // 'cssmin',
-    // 'sails-linker:prodJs',
-    // 'sails-linker:prodStyles',
-    // 'sails-linker:devTpl',
-    // 'sails-linker:prodJsJADE',
-    // 'sails-linker:prodStylesJADE',
-    // 'sails-linker:devTplJADE'
+    'clean:dev',
+    'jst:dev',
+    'less:dev',
+    'copy:dev',
+    'coffee:dev',
+    'concat',
+    'uglify',
+    'cssmin',
+    'sails-linker:prodJs',
+    'sails-linker:prodStyles',
+    'sails-linker:devTpl',
+    'sails-linker:prodJsJADE',
+    'sails-linker:prodStylesJADE',
+    'sails-linker:devTplJADE'
   ]);
 
   grunt.registerTask('test', [
